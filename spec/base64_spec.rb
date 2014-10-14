6378204bb031d42bbb082e3cdeaa9df0b0ae259c
drrb <drrrrrrrrrrrb@gmail.com>
2014-10-14 18:23:41 +1100

    Added basic `base64` decoding

--- spec/base64_spec.rb
+++ spec/base64_spec.rb
@@ -2,6 +2,20 @@ require "spec_helper"
 
 describe "base64" do
   it "encodes stdin" do
-    expect("target/base64 <<< hello").to emit(stdout: "aGVsbG8K")
+    pending
+    expect("target/base64 <<< hello").to emit(stdout: "aGVsbG8K\n")
+  end
+
+  describe "--decode" do
+    it "decodes stdin" do
+      expect("target/base64 --decode <<< aGVsbG8K").to emit(stdout: "hello\n")
+    end
+
+    context "when stdin can't be decoded" do
+      it "prints question marks for invalid blocks" do
+        pending
+        expect("target/base64 --decode <<< hello").to emit(stdout: "??e\n")
+      end
+    end
   end
 end
--- src/base64.rs
+++ src/base64.rs
@@ -1,16 +1,18 @@
+extern crate getopts;
 extern crate serialize;
 
 use std::os;
 use std::io::stdio;
-use serialize::base64::{ToBase64,Config,Standard};
+use serialize::base64::{FromBase64,ToBase64,Config,Standard};
+use getopts::{optflag, getopts};
 
 fn main() {
     let exit_status = run(os::args());
     os::set_exit_status(exit_status);
 }
 
-fn print(message: String) {
-    let result = stdio::stdout().write(message.as_bytes());
+fn print(bytes: &[u8]) {
+    let result = stdio::stdout().write(bytes);
     match result {
         Ok(_) => (),
         Err(failure) => fail!(format!("Failed to write to stdout: {}", failure))
@@ -18,8 +20,23 @@ fn print(message: String) {
 }
 
 fn run(args: Vec<String>) -> int {
-    let input = stdio::stdin().read_to_string().ok().expect("Failed to read from stdin");
-    let output = input.as_bytes().to_base64(Config { char_set: Standard, pad: true, line_length: None});
-    print(output);
+    let parameters = [
+        optflag("D", "decode", "decodes input"),
+    ];
+
+    let options = match getopts(args.tail(), parameters) {
+        Ok(options) => options,
+        Err(failure) => fail!(failure.to_string())
+    };
+
+    let stdin = stdio::stdin().read_to_string().ok().expect("Failed to read from stdin");
+    let input = stdin.as_bytes();
+    if options.opt_present("D") {
+        let output = input.from_base64().ok().expect("Failed to decode");
+        print(output.as_slice());
+    } else {
+        let output = input.to_base64(Config { char_set: Standard, pad: true, line_length: None});
+        print(output.as_bytes());
+    }
     return 0;
 }
