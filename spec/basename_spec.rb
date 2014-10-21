c0955b6dd939bd36478fe41f86f56f96c0c74d6c
drrb <drrrrrrrrrrrb@gmail.com>
2014-10-21 21:47:52 +1100

    Added suffix stripping to `basename`

--- spec/basename_spec.rb
+++ spec/basename_spec.rb
@@ -4,4 +4,24 @@ describe "basename" do
   it "prints the last segment of the provided path" do
     expect("target/basename /path/to/file.txt").to emit(stdout: "file.txt\n")
   end
+
+  context "with a suffix provided" do
+    context "when the basename ends with the suffix" do
+      it "prints the basename without the suffix" do
+        expect("target/basename /path/to/file.txt .txt").to emit(stdout: "file\n")
+      end
+    end
+
+    context "when the basename doesn't end with the suffix" do
+      it "prints the basename" do
+        expect("target/basename /path/to/file.txt .bin").to emit(stdout: "file.txt\n")
+      end
+    end
+
+    context "when the basename exactly matches the suffix" do
+      it "prints the basename" do
+        expect("target/basename /path/to/.txt .txt").to emit(stdout: ".txt\n")
+      end
+    end
+  end
 end
--- src/basename.rs
+++ src/basename.rs
@@ -1,4 +1,5 @@
 use std::os;
+use std::str;
 
 fn main() {
     let exit_status = run(os::args());
@@ -6,8 +7,24 @@ fn main() {
 }
 
 fn run(args: Vec<String>) -> int {
-    let path = Path::new(args[1].as_bytes());
-    let last_segment = path.components().last().unwrap();
-    println!("{}", String::from_utf8_lossy(last_segment));
+    let path = Path::new(args[1].as_slice());
+    let suffix_to_strip = match args.len() {
+        2 => None,
+        _ => Some(args[2].as_slice())
+    };
+
+    let last_segment = str::from_utf8(path.components().last().unwrap()).unwrap();
+
+    let basename = match suffix_to_strip {
+        None => last_segment,
+        Some(suffix) => {
+            if last_segment.ends_with(suffix) && last_segment != suffix {
+                last_segment.slice_to(last_segment.len() - suffix.len())
+            } else {
+                last_segment
+            }
+        }
+    };
+    println!("{}", basename);
     return 0;
 }
